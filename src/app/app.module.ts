6ede7eb15b07bd55d86f45874eff7a6ce7ab1b21
dotJManuel <dotJManuel@outlook.com>
2023-12-12 12:26:03 -0600

    se genero agrego el menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
 
-import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
@@ -13,8 +14,9 @@ import { ButtonModule } from 'primeng/button';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
 
-    ButtonModule
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
