6e7cfd4ed43c5a44e8c6f5147621e6920c0d5aed
dotJManuel <dotJManuel@outlook.com>
2023-12-13 23:38:28 -0600

    se agrego el json y keyvalue pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -31,4 +31,11 @@ export class UncommonPageComponent {
     deleteClient(): void {
         this.clients.shift();
     }
+
+    // KeyValue Pipe
+    public person = {
+        name: 'Fernando',
+        age: 36,
+        address: 'Ottawa, Canada'
+    }
 }
