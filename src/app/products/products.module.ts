2b22f23b249d46657a215362a6f73bf74b8dcfa6
dotJManuel <dotJManuel@outlook.com>
2023-12-14 23:39:28 -0600

    se agrego pipes personalizados

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -7,19 +7,27 @@ import { NumbersPageComponent } from './pages/numbers-page/numbers-page.componen
 import { UncommonPageComponent } from './pages/uncommon-page/uncommon-page.component';
 import { PrimeNgModule } from '../prime-ng/prime-ng.module';
 import { OrderComponent } from './pages/order/order.component';
+import { ToggleCasePipe } from './pipes/toggle-case.pipe';
+import { CanFlyPipe } from './pipes/can-fly.pipe';
+import { SortByPipe } from './pipes/sort-by.pipe';
 
 
 @NgModule({
-  declarations: [
-    BasicsPageComponent,
-    NumbersPageComponent,
-    UncommonPageComponent,
-    OrderComponent
-  ],
-  imports: [
-    CommonModule,
-    ProductsRoutingModule,
-    PrimeNgModule
-  ]
+    declarations: [
+        BasicsPageComponent,
+        NumbersPageComponent,
+        UncommonPageComponent,
+        OrderComponent,
+
+        // Pipes
+        ToggleCasePipe,
+        CanFlyPipe,
+        SortByPipe
+    ],
+    imports: [
+        CommonModule,
+        ProductsRoutingModule,
+        PrimeNgModule
+    ]
 })
 export class ProductsModule { }
